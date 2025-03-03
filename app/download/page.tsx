102941e364f876605e46ecfdf93348648affb508
Grian Gajila <griangajila2@gmail.com>
2025-03-03 23:13:02 +0800

    wip

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -16,9 +16,9 @@ export default function Download() {
 		}
 	}, [count]);
 	return (
-		<div className="flex justify-center items-center h-screen w-full bg-[#015d9c] font-[family-name:var(--font-geist-sans)]">
+		<div className="flex justify-center items-center h-screen w-full  font-[family-name:var(--font-geist-sans)]">
 			<title>Download</title>
-			<div className="flex-col text-white font-bold text-4xl justify-center items-center text-center">
+			<div className="flex-col font-bold text-4xl justify-center items-center text-center">
 				<h1 className="mb-5">Wait a second!</h1>
 				<h2 className="text-2xl text-amber-300">
 					{count === 0 ? "Thank you!" : count}
