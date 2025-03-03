99f7cbe50c7764012fe02829d4d67b55ba001912
Grian Gajila <griangajila2@gmail.com>
2025-03-04 00:22:45 +0800

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,12 @@ export default function Home() {
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<Button>
-				<a href="download" target="_blank" title="download app">
+				<a
+					href="e-sugar-rush.apk"
+					download="e-sugar-rush.apk"
+					target="_blank"
+					title="download app"
+				>
 					Download App
 				</a>
 			</Button>
